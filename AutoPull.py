0928e24a96e921dacd4d64cf0c1369b8812e6376
Mohamed Nasraoui <mednasraoui.mn@gmail.com>
2023-07-04 23:56:15 +0100

    Adding AllowedRepos functionality in order to disable certain repositories if needed

--- AutoPull.py
+++ AutoPull.py
@@ -2,20 +2,35 @@ from os import listdir, chdir
 from os.path import isdir
 from subprocess import run
 
-config_src = "/etc/AutoPull.conf"
+ConfigFolder = "/etc/AutoPull/"
+allowed_src = "/etc/AutoPull/AllowedRepos" 
+config_src = "/etc/AutoPull/AutoPull.conf"
 default_config = "#This file should be modified with caution, misconfiguration can result in complete disfunction of the program\n#The src value should point to the folder containing all the GitHub repositories that the user wishes to Auto-Update\n\n[Settings]\n\nsrc = {}\nVerboseGit = on\nDebugging = off"
 
-FileNotFoundErrorMessage = "\n--------------------------------------------\nAutoPull.conf doesn't exist ! Recreating a default configuration file at {}".format(config_src)
-WritePermisionMessage = "Couldn't write the config file to {} ! You don't have write permission, switch to root ".format(config_src)
-FileCreationMessage = "Config file successfully created !\nRestart AutoPull !"
+FileNotFoundErrorMessage = "\n--------------------------------------------\n{} doesn't exist ! Recreating a default file at {}"
+WritePermisionMessage = "Couldn't write the {} ! You don't have write permission, switch to root "
+FileCreationMessage = "{} successfully created !\nRestart AutoPull !"
 NotDirectoryWarningMessage = "Warning : {} isn't a directory, it will be skipped !"
 InputMessage = "Please enter the path of the folder containing the GitHub repositories : "
+NewFolderDetection = "AutoPull detected a new Git repository in {}"
 
+# Recreating the Configuration Folder if it doesn't exist
+if isdir(ConfigFolder)==False:
+    try:
+        run(["mkdir", ConfigFolder])
+    except Exception as e: # Problem in the creation (Write Permission)
+        print(WritePermisionMessage.format("config folder to " + ConfigFolder))
+        print(e)
+        quit()
+    else:
+        print(FileCreationMessage.format("config folder").replace("\nRestart AutoPull !", ""))
+
+# Retrieving Configuration From AutoPull.conf
 try:
-    with open(config_src, 'r') as config : # Opening Config.txt to get the settings
+    with open(config_src, 'r') as config : # Opening AutoPull.conf to get the settings
         Lines = config.readlines()
 except FileNotFoundError: # Recreating Config file if it doesn't exist
-    print(FileNotFoundErrorMessage)
+    print(FileNotFoundErrorMessage.format("AutoPull.conf", config_src))
     try:
         with open(config_src, 'x+') as config:
             src = input(InputMessage)
@@ -25,11 +40,12 @@ except FileNotFoundError: # Recreating Config file if it doesn't exist
             if src[-1]!="/":
                 src = src + "/"
             config.write(default_config.format(src))
-    except Exception: # Problem in the creation (Write Permission)
-        print(WritePermisionMessage)
+    except Exception as e: # Problem in the creation (Write Permission)
+        print(WritePermisionMessage.format("config file to " + config_src))
+        print(e)
         quit()
     else:
-        print(FileCreationMessage)
+        print(FileCreationMessage.format("AutoPull.conf"))
         quit()
 else:
     src = Lines[5].replace("src = ", "").replace("\n", "").replace("'", "")
@@ -38,8 +54,11 @@ else:
     if debugging:
         print("src = {}\nVerboseGit = {}\nDebugging = {}\n".format(src, verbose, debugging)) # For debugging purpose 
 
+# Verifying Directories Validity And Preparing Repos List
 BuildsFolder = listdir(src)
 repos = []
+allowed_repos = []
+not_allowed_repos = []
 
 if debugging:
     print(BuildsFolder) # For debugging purpose
@@ -49,17 +68,71 @@ for name in BuildsFolder:
         print(path) # For debugging purpose
     IsDir = isdir(path)
     if IsDir:
-        repos.append(path+"/")
+        repos.append(path)
     else:
         print(NotDirectoryWarningMessage.format(path))
 if debugging:
     print(repos) # For debugging purpose
 
-for repo in repos :
-    repo_name = repo.replace(src, "").replace("/", "")
-    print("\n--------------------------------------\nUpdating {} : ".format(repo_name))
-    chdir(repo)
-    if verbose:
-        run(["git", "pull", "--verbose"])
+try:
+    with open(allowed_src, 'r') as AllowedRepos : # Opening AllowedRepos to get the specific GitHub repositories to update
+        AllowedReposLines = AllowedRepos.readlines()
+except FileNotFoundError: # Recreating AllowedRepos file if it doesn't exist
+    print(FileNotFoundErrorMessage.format("AllowedRepos", allowed_src))
+    try:
+        with open(allowed_src, 'x+') as AllowedRepos:
+            for repo in repos:
+                Value = input("Allow updating " + repo + " (Y/n) : ")
+                if Value.upper()=="Y":
+                    AllowedRepos.write(repo + " = Allowed\n")
+                else:
+                    AllowedRepos.write(repo + " = NotAllowed\n")
+
+    except Exception: # Problem in the creation (Write Permission)
+        print(WritePermisionMessage.format("AllowedRepos file to " + allowed_src))
+        quit()
     else:
-        run(["git", "pull"])
+        print(FileCreationMessage.format("AllowedRepos"))
+        quit()
+else:
+    for line in AllowedReposLines: # Check if the repo value is True or False
+        repo_boolean_value = line.replace("\n", "").split(" = ")
+        if repo_boolean_value[1]=="Allowed":
+            allowed_repos.append(repo_boolean_value[0])
+        elif repo_boolean_value[1]=="NotAllowed":
+            not_allowed_repos.append(repo_boolean_value[0])
+    if debugging:
+        print(allowed_repos) # For debugging purpose 
+
+# Adding the repositories non present in the src folder to the repos list
+for repo in allowed_repos:
+    if repo not in repos:
+        repos.append(repo)
+
+# Updating The Repos
+for repo in repos :
+    if src in repo :
+        repo_name = repo.replace(src, "").replace("/", "")
+    elif src not in repo :    
+        repo_name = repo.split("/")[-1]
+    if repo in allowed_repos:
+        print("\n--------------------------------------\nUpdating {} : ".format(repo_name))
+        chdir(repo)
+        if verbose:
+            run(["git", "pull", "--verbose"])
+        else:
+            run(["git", "pull"])
+    elif repo not in not_allowed_repos:
+        print("\n" + NewFolderDetection.format(src))
+        add = input("Would you like to allow AutoPull to update {} (Y/n) : ".format(repo_name))
+        try:
+            with open(allowed_src, "a") as AllowedRepos:
+                if add.upper()=="Y":
+                    AllowedRepos.write(repo + " = Allowed\n")
+                else:
+                    AllowedRepos.write(repo + " = NotAllowed\n")
+        except PermissionError:
+            print(WritePermisionMessage.format("AllowedRepos file to " + allowed_src))
+            quit()
+        else:
+            print("The folder has been successfully configured !")
--- README.md
+++ README.md
@@ -13,14 +13,22 @@ AutoPull is a Python script that manages a configuration file and automates the 
 
 2. Configure the `AutoPull.conf` file:
  * Automatic Configuration :
-   (You need to run the script as root to create the /etc/AutoPull.conf automatically. If you don't have root access, you can change AutoPull.conf path in the script `config_src` variable.)
+   (You need to run the script as root to create the /etc/AutoPull/AutoPull.conf automatically. If you don't have root access, you can change AutoPull.conf path in the script `config_src` variable.)
    - If the `AutoPull.conf` file doesn't exist, the script will recreate it with default settings.
  * Manual Configuration :
-   (You can copy the default configuration presented below. )
+   (You can copy the default configuration presented below.)
    - Create the `AutoPull.conf` at the chosen path (config_src).
    - Open the `AutoPull.conf` file in a text editor.
    - Modify the `src` value to specify the path of the folder containing the GitHub repositories you want to update.
    - Optionally, adjust the `VerboseGit` and `Debugging` settings according to your preferences.
+
+3. First run of the script:
+
+   ```
+   python AutoPull.py
+   ```
+
+   The script will read the configuration file, retrieve the repository paths and then ask the user for each repository found if they want to allow the Auto-update.
 
 4. Run the script:
 
@@ -28,7 +36,8 @@ AutoPull is a Python script that manages a configuration file and automates the 
    python AutoPull.py
    ```
 
-   The script will read the configuration file, retrieve the repository paths, and update each repository using the `git pull` command.
+   The script will read the configuration file, retrieve the repository paths and the allowed paths in order to compare them and add new folders if they are
+   detected, and then, it will update the allowed repositories using the `git pull` command.
 
 5. Review the output:
 
@@ -37,7 +46,7 @@ AutoPull is a Python script that manages a configuration file and automates the 
 
 ## Configuration
 
-The configuration file (`AutoPull.conf`) is used to specify the settings for AutoPull. Here's an example of the default configuration:
+1. The configuration file (`AutoPull.conf`) is used to specify the settings for AutoPull. Here's an example of the default configuration:
 
 ```
 #This file should be modified with caution, misconfiguration can result in complete disfunction of the program
@@ -54,10 +63,16 @@ Debugging = off
 - `VerboseGit`: Determines whether to display verbose output from the `git pull` command.
 - `Debugging`: Enables or disables debugging mode for additional output and information.
 
+2. `AllowedRepos` file contains all the repositories that AutoPull detected in the src folder specified in `AutoPull.conf`. The user can disable the update of a
+specific Git repository by changing the `Allowed` value stated next to the repository's path in this file. As a result, the script will ignore this repository.
+N.B : - AutoPull will detect new folders and ask the user if they want to allow the Auto-update
+      - You can include folders not present in the src folder by adding their path in `AllowedRepos` followed by ` = Allowed`. Make sure not to change the file 
+      structure. This can lead to a break of the scriptn read the Troubleshooting section if this occurs .
+
 ## Troubleshooting
 
 - If the `AutoPull.conf` file is missing, and you don't have write permission to recreate it, make sure to switch to the root user or an account with sufficient privileges.
-- If any errors occur during the execution of the script, ensure that you have the necessary permissions and that Git is properly installed and configured.
+- If any errors occur during the execution of the script, ensure that you have the necessary permissions and that Git is properly installed and configured. If it's the case, then, the problem is due to the configuration of the script. All you have to do is deleting AutoPull.conf and AllowedRepos.
 
 ## Limitations
 
